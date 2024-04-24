5ac04e345f4c634773b69402c42704e10ecefa05
Nyakio-Eva <evalkariuki@gmail.com>
2024-04-24 08:38:38 +0300

    completed assignment

--- server/app.py
+++ server/app.py
@@ -1,7 +1,8 @@
 #!/usr/bin/env python3
 
-from flask import request, session
+from flask import request, session,jsonify
 from flask_restful import Resource
+
 
 from config import app, db, api
 from models import User
@@ -16,28 +17,54 @@ class ClearSession(Resource):
         return {}, 204
 
 class Signup(Resource):
-    
     def post(self):
-        json = request.get_json()
-        user = User(
-            username=json['username']
-        )
-        user.password_hash = json['password']
+        json_data = request.get_json()
+        username = json_data.get('username')
+        password = json_data.get('password')
+            
+        user = User(username=username)
+        user.password_hash = password 
+
         db.session.add(user)
         db.session.commit()
+
         return user.to_dict(), 201
+     
 
 class CheckSession(Resource):
-    pass
+    def get(self):
+        user_id = session.get('user_id')
+        if user_id:
+           user = User.query.filter(User.id == user_id).first()
+           if user:
+               return user.to_dict(), 200
+           
+        return {}, 204
 
 class Login(Resource):
-    pass
+    def post(self):
+        json_data = request.get_json()
+        username = json_data.get('username')
+        
+
+        user = User.query.filter_by(username=username).first()
+
+        session['user_id'] = user.id
+
+        return user.to_dict(), 200
 
 class Logout(Resource):
-    pass
+    def delete(self):
+        session.pop('user_id', None)
+        return {}, 204
+    
+
 
 api.add_resource(ClearSession, '/clear', endpoint='clear')
 api.add_resource(Signup, '/signup', endpoint='signup')
+api.add_resource(CheckSession, '/check_session', endpoint='check_session')
+api.add_resource(Login, '/login', endpoint='login')
+api.add_resource(Logout, '/logout', endpoint='logout')
 
 if __name__ == '__main__':
     app.run(port=5555, debug=True)
